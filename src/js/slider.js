d887df3d832ded952fe853ed8f0fe2a45bd24e3c
E-Korsakova <evild3vilcat@gmail.com>
2023-11-21 14:34:29 +0300

    исправлена пагинация и уменьшены отступы между слайдами

--- src/js/slider.js
+++ src/js/slider.js
@@ -17,33 +17,162 @@ function swiper() {
     return check()
   }
 
-  swiperResize('(max-width: 374px)', '.swiper', {
+  swiperResize('(min-width: 320px)', '.repairable-brands__swiper', {
     loop: true,
     spaceBetween: 15,
-    slidesPerView: 1.2,
     pagination: {
-      el: '.swiper-pagination',
+      el: '.swiper-pagination--brands',
       clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1.3
+      },
+      340: {
+        slidesPerView: 1.4
+      },
+      375: {
+        slidesPerView: 1.5
+      },
+      400: {
+        slidesPerView: 1.6
+      },
+      425: {
+        slidesPerView: 1.7
+      },
+      450: {
+        slidesPerView: 1.8
+      },
+      475: {
+        slidesPerView: 1.9
+      },
+      500: {
+        slidesPerView: 2
+      },
+      530: {
+        slidesPerView: 2.1
+      },
+      560: {
+        slidesPerView: 2.2
+      },
+      600: {
+        slidesPerView: 2.3
+      },
+      650: {
+        slidesPerView: 2.5
+      },
+      700: {
+        slidesPerView: 2.7
+      },
+      750: {
+        slidesPerView: 2.9
+      }
     }
   })
 
-  swiperResize('(max-width: 424px)', '.swiper', {
+  swiperResize('(min-width: 320px)', '.repairable-equipment__swiper', {
     loop: true,
     spaceBetween: 15,
-    slidesPerView: 1.5,
     pagination: {
-      el: '.swiper-pagination',
+      el: '.swiper-pagination--equipment',
       clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1.3
+      },
+      340: {
+        slidesPerView: 1.4
+      },
+      375: {
+        slidesPerView: 1.5
+      },
+      400: {
+        slidesPerView: 1.6
+      },
+      425: {
+        slidesPerView: 1.7
+      },
+      450: {
+        slidesPerView: 1.8
+      },
+      475: {
+        slidesPerView: 1.9
+      },
+      500: {
+        slidesPerView: 2
+      },
+      530: {
+        slidesPerView: 2.1
+      },
+      560: {
+        slidesPerView: 2.2
+      },
+      600: {
+        slidesPerView: 2.3
+      },
+      650: {
+        slidesPerView: 2.5
+      },
+      700: {
+        slidesPerView: 2.7
+      },
+      750: {
+        slidesPerView: 2.9
+      }
     }
   })
 
-  swiperResize('(max-width: 767px)', '.swiper', {
+  swiperResize('(min-width: 320px)', '.price__swiper', {
     loop: true,
     spaceBetween: 15,
-    slidesPerView: 1.75,
     pagination: {
-      el: '.swiper-pagination',
+      el: '.swiper-pagination--price',
       clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1.2
+      },
+      340: {
+        slidesPerView: 1.3
+      },
+      375: {
+        slidesPerView: 1.4
+      },
+      400: {
+        slidesPerView: 1.5
+      },
+      425: {
+        slidesPerView: 1.6
+      },
+      450: {
+        slidesPerView: 1.7
+      },
+      475: {
+        slidesPerView: 1.8
+      },
+      500: {
+        slidesPerView: 1.9
+      },
+      530: {
+        slidesPerView: 2
+      },
+      560: {
+        slidesPerView: 2.1
+      },
+      600: {
+        slidesPerView: 2.2
+      },
+      650: {
+        slidesPerView: 2.4
+      },
+      700: {
+        slidesPerView: 2.6
+      },
+      750: {
+        slidesPerView: 2.8
+      }
     }
   })
 }
